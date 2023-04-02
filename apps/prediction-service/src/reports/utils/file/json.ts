66c3236867bfa8543fe2afc1ced367c6ebf4f968
Maksym Voloshko <maksym.voloshko@gmail.com>
2023-04-02 23:23:26 +0300

    tf-idf to csv implemented

--- apps/prediction-service/src/reports/utils/file/json.ts
+++ apps/prediction-service/src/reports/utils/file/json.ts
@@ -2,6 +2,7 @@ import { Document } from "../../document/DocumentManager";
 import fs from "fs";
 import path from "path";
 import { getAllTerms } from "../../tf-idf/utils";
+const jsonStream = require("JSONStream");
 
 const PARSED_REPORTS_FILE_NAME = "parsed_reports.json";
 const ALL_TERMS_FILE_NAME = "all_terms.json";
@@ -10,9 +11,18 @@ export const savePreprocessedToJson = (
   documents: Document[],
   fileName: string = PARSED_REPORTS_FILE_NAME
 ) => {
-  fs.writeFile(getFilePath(fileName), JSON.stringify(documents), () => {
-    console.log("JSON parsed_reports file successfully written!");
-  });
+  const transformStream = jsonStream.stringify();
+  const outputStream = fs.createWriteStream(getFilePath(fileName));
+  transformStream.pipe(outputStream);
+  documents.forEach(transformStream.write);
+  transformStream.end();
+
+  outputStream.on("finish", () => {
+    console.log(`JSON ${fileName} file successfully written!`);
+  })
+  // fs.writeFile(getFilePath(fileName), JSON.stringify(documents), () => {
+  //   console.log("JSON parsed_reports file successfully written!");
+  // });
 };
 
 export const saveAllTermsToJson = (documents: Document[]) => {
