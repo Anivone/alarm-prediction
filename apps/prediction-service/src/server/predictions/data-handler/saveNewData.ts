7ede261cb45f14e35452c0bbd99909ba96c56b0f
Maksym Voloshko <maksym.voloshko@gmail.com>
2023-04-25 17:17:49 +0300

    deployment preparation v4

--- apps/prediction-service/src/server/predictions/data-handler/saveNewData.ts
+++ apps/prediction-service/src/server/predictions/data-handler/saveNewData.ts
@@ -3,8 +3,9 @@ import { writeCurrentWeather } from "./write/writeCurrentWeather";
 import { writeCurrentIsw } from "./write/writeCurrentIsw";
 
 export const saveNewData = async (regionName: string) => {
-  await writeCurrentAlarms(regionName);
-  writeCurrentWeather(regionName).then(async () => {
-    await writeCurrentIsw();
+  writeCurrentAlarms(regionName).then(() => {
+    writeCurrentWeather(regionName).then(async () => {
+      await writeCurrentIsw();
+    });
   });
-}
+};
