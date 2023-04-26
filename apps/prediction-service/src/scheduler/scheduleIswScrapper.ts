e2370decae464a5a93ddb8830c3d587382e50dff
Maksym Voloshko <maksym.voloshko@gmail.com>
2023-04-26 15:55:23 +0300

    deployment preparation v5

--- apps/prediction-service/src/scheduler/scheduleIswScrapper.ts
+++ apps/prediction-service/src/scheduler/scheduleIswScrapper.ts
@@ -1,5 +1,6 @@
 import { RecurrenceRule, scheduleJob } from "node-schedule";
 import { scrapIswReports } from "../data-manager/reports/utils";
+import { evaluateTfIdf } from "../data-manager/reports";
 
 export const scheduleIswScrapper = () => {
   const rule = new RecurrenceRule(
@@ -18,5 +19,6 @@ export const scheduleIswScrapper = () => {
     console.log("IswScrapper executed");
 
     await scrapIswReports();
+    await evaluateTfIdf();
   });
 };
